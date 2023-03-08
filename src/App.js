77a1da6df8de9d080f4b24b39fb010c9ae098e25
Ayan Gorain <ayan.gorain2021@vitbhopal.ac.in>
2023-03-08 22:45:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <dix>
+          <h1>EProcure</h1>
+          <h4>Welcome to eProcure</h4>
+          <h4>We will start working it as soon as possible</h4>
+          <h4>We will start working it as soon as possible</h4>
+         
+
+        </dix>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
